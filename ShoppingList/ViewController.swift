a6e3815e985d6d444d081c06ddadcb955b070599
masummodi <masum.modi@yobored.com>
2020-10-29 22:33:02 -0400

    - Bind static 5 tableViewCell to tableview programatically

--- ShoppingList/ViewController.swift
+++ ShoppingList/ViewController.swift
@@ -10,11 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        tableView.dataSource = self
     }
+    
+}
 
-
+extension ViewController: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "shoppingListItem", for: indexPath)
+        //cell.textLabel?.text = "Hellow word"
+        return cell
+    }
 }
 
